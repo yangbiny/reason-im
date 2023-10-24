b3f49b7fa6b7f47eed425df450145b7fa9f3c3ee
tingfa.yang <tingfa.yang@duitang.com>
2023-10-24 18:08:38 +0800

    add group

--- internal/service/group_service.go
+++ internal/service/group_service.go
@@ -2,7 +2,7 @@ package service
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/yangbiny/reason-commons/err"
+	apierror "github.com/yangbiny/reason-commons/err"
 	"reason-im/internal/repo"
 	"reason-im/pkg/model"
 )
@@ -19,9 +19,23 @@ func NewGroupService(groupDao repo.GroupDao) GroupService {
 	}
 }
 
-func (service GroupService) NewGroup(ctx *gin.Context, cmd *CreateGroupCmd) (*Group, *err.ApiError) {
-	panic("implement me")
+func (service GroupService) NewGroup(ctx *gin.Context, cmd *CreateGroupCmd) (*Group, *apierror.ApiError) {
+	group := &Group{
+		Name:           cmd.Name,
+		Description:    "",
+		GroupType:      model.GroupType(cmd.Type),
+		GroupMemberCnt: 1,
+	}
+	newGroup, err := service.groupDao.NewGroup(ctx, group)
+	if err != nil {
+		return nil, apierror.WhenServiceError(err)
+	}
+	return newGroup, nil
+
 }
 
 type CreateGroupCmd struct {
+	UserId int64  `login_user_id:"user_id"`
+	Name   string `json:"name"`
+	Type   int    `json:"type"`
 }
