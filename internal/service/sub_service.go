4aa8738130d805de86cf32d1195661a0aa2f97ea
tingfa.yang <tingfa.yang@duitang.com>
2023-11-23 15:56:16 +0800

    add sub

--- internal/service/sub_service.go
+++ internal/service/sub_service.go
@@ -6,9 +6,24 @@ type channel struct {
 	ChannelId string
 }
 
-func SubChannel(channel string, userId int64) {
+// ApplySubToken 申请订阅的 token
+func ApplySubToken(channel string, userId int64) string {
+	return ""
+}
+
+func SubChannel(channel string, userId int64, token string) {
 	c := channelMap[channel]
 	if c == nil {
 		return
 	}
+	// 验证 token 是否合法
+}
+
+func UnSubChannel(channel string, userId int64) {
+
+}
+
+// SendMsgToChannel 发送消息到频道。订阅者 可以在消息 发送后，接受到发送的消息
+func SendMsgToChannel(channel string, msg string) {
+
 }
